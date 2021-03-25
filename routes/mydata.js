2351228aa8fb717005a9efdd083a771e764195d3
MehalS542312 <77812392+MehalS542312@users.noreply.github.com>
2021-03-24 19:04:59 -0500

    updated mydata.js file with title as myname

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mehal Reddy Mula' });
+  res.render('mydata', { title: 'Mehal Reddy Mula' });
 });
 
 module.exports = router;
